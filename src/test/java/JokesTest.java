62dc2af36678492e41d6e1c1fee9b0d755f8b332
Saulo Contente <scontente@aliancahospitalar.com>
2020-06-12 23:19:51 -0300

    Alter variables names

--- src/test/java/JokesTest.java
+++ src/test/java/JokesTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class JokesTest {
 
     private String url = "https://api.jokes.one";
-    private String resourceJod = "/jod";
+    private String resourceEndpointJod = "/jod";
 
     @Test
     public void getJokeOfToday() {
         ExtractableResponse<Response> extracted =
                 when()
-                .get(url+resourceJod)
+                .get(url+resourceEndpointJod)
                 .then()
                 .statusCode(200)
                 .extract()
@@ -23,5 +24,4 @@ public class JokesTest {
         List<String> jokeOfDay = extracted.path("contents.jokes.joke.text");
         System.out.println(jokeOfDay);
     }
-
 }
